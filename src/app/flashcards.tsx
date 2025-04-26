4e2d8cf051df5cbb23735719f4240cc4da601fcd
Charlie Brinicombe <55358291+cbrinicombe13@users.noreply.github.com>
2025-04-26 23:09:32 +0100

    Add interactivity

--- src/app/flashcards.tsx
+++ src/app/flashcards.tsx
@@ -1,21 +1,48 @@
-import { Carousel, CarouselContent, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
+'use client';
+
+import { Carousel, CarouselContent, CarouselPrevious, CarouselNext, type CarouselApi } from "@/components/ui/carousel";
 import { IFlashcard } from "@/types/flashcard";
 import Flashcard from "./flashcard";
+import { useEffect, useState } from "react";
+import { Progress } from "@/components/ui/progress";
 
 interface Props {
     flashcards: IFlashcard[];
 }
 
 export default function Flashcards({ flashcards }: Props) {
+    const [flashIndex, setFlashIndex] = useState(0);
+    const [api, setApi] = useState<CarouselApi>();
+
+    useEffect(() => {
+        if (!api) {
+            return;
+        }
+
+        // Initialize the flash index
+        setFlashIndex(api.selectedScrollSnap() + 1);
+
+        // Update the flash index when the carousel is scrolled
+        api.on("select", () => {
+            setFlashIndex(api.selectedScrollSnap() + 1);
+        });
+    }, [api]);
+
     return (
-        <Carousel className="w-full max-w-md">
-            <CarouselContent>
-                {flashcards.map((flashcard) => (
-                    <Flashcard key={flashcard.id} flashcard={flashcard} />
-                ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-        </Carousel>
+        <div className="flex flex-col items-center justify-center gap-4 max-w-md">
+            <Progress value={flashIndex / flashcards.length * 100} />
+            <Carousel className="w-full" setApi={setApi}>
+                <CarouselContent>
+                    {flashcards.map((flashcard) => (
+                        <Flashcard
+                            key={flashcard.id}
+                            flashcard={flashcard}
+                        />
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </div>
     )
 }
