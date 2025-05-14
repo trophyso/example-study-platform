1a319d3fd3194391f75f3f166df2a7defc2b0d60
Charlie Brinicombe <55358291+cbrinicombe13@users.noreply.github.com>
2025-05-14 11:39:23 +0100

    Display powered by element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import { flashcards } from "@/data";
 import Flashcards from "./flashcards";
 import { Toaster } from "@/components/ui/sonner"
 import StudyJourney from "./study-journey";
-
+import PoweredBy from "./powered-by";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen relative">
       <StudyJourney />
       <Flashcards flashcards={flashcards} />
+      <PoweredBy />
       <Toaster />
     </div>
   )
