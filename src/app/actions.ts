7a4c618847b35a28b66e4e22b0f30a00e25e1af7
Charlie Brinicombe <55358291+cbrinicombe13@users.noreply.github.com>
2025-07-03 12:29:35 +0100

    Formattign and update types

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { TrophyApiClient } from '@trophyso/node';
-import { EventResponse, AchievementResponse, StreakResponse } from '@trophyso/node/api';
+import { EventResponse, StreakResponse, CompletedAchievementResponse } from '@trophyso/node/api';
 
 const FLASHCARDS_VIEWED_METRIC_KEY = "flashcards-viewed";
 
@@ -32,7 +32,7 @@ export async function viewFlashcard(userId: string): Promise<EventResponse | nul
  * Get the achievements for a user
  * @returns The achievements for the user
  */
-export async function getAchievements(userId: string): Promise<AchievementResponse[] | null> {
+export async function getAchievements(userId: string): Promise<CompletedAchievementResponse[] | null> {
     try {
         return await trophy.users.allAchievements(userId);
     } catch (error) {
--- src/app/study-journey.tsx
+++ src/app/study-journey.tsx
@@ -1,5 +1,5 @@
 import { Separator } from "@/components/ui/separator";
-import { AchievementResponse, StreakResponse } from "@trophyso/node/api";
+import { CompletedAchievementResponse, StreakResponse } from "@trophyso/node/api";
 import { Flame, GraduationCap } from "lucide-react";
 import Image from "next/image";
 import dayjs from 'dayjs';
@@ -11,210 +11,210 @@ import { getUserId } from "@/lib/user";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function StudyJourney() {
-    const [data, setData] = useState<{
-        achievements: AchievementResponse[] | null;
-        streak: StreakResponse | null;
-    }>({
-        achievements: null,
-        streak: null
-    });
-    const [open, setOpen] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<{
+    achievements: CompletedAchievementResponse[] | null;
+    streak: StreakResponse | null;
+  }>({
+    achievements: null,
+    streak: null
+  });
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        if (!open) {
-            return;
-        }
+  useEffect(() => {
+    if (!open) {
+      return;
+    }
 
-        const fetchData = async () => {
-            const userId = getUserId();
+    const fetchData = async () => {
+      const userId = getUserId();
 
-            setLoading(true);
+      setLoading(true);
 
-            const achievements = await getAchievements(userId);
-            const streak = await getStreak(userId);
+      const achievements = await getAchievements(userId);
+      const streak = await getStreak(userId);
 
-            setData({
-                achievements,
-                streak
-            });
+      setData({
+        achievements,
+        streak
+      });
 
-            setLoading(false);
-        };
-        fetchData();
-    }, [open]);
+      setLoading(false);
+    };
+    fetchData();
+  }, [open]);
 
-    const adjustedStreakHistory = useMemo(() => {
-        const sundayOffset = 7 - (new Date().getDay() + 6) % 7;
+  const adjustedStreakHistory = useMemo(() => {
+    const sundayOffset = 7 - (new Date().getDay() + 6) % 7;
 
-        return data.streak?.streakHistory?.slice(
-            sundayOffset - 1,
-            data.streak?.streakHistory?.length
-        ) || Array(14).fill(null);
-    }, [data.streak]);
+    return data.streak?.streakHistory?.slice(
+      sundayOffset - 1,
+      data.streak?.streakHistory?.length
+    ) || Array(14).fill(null);
+  }, [data.streak]);
 
-    return (
-        <div className="absolute top-10 right-10 z-50 cursor-pointer">
-            <Dialog open={open} onOpenChange={setOpen}>
-                <DialogTrigger>
-                    <div className="h-12 w-12 cursor-pointer duration-100 border-1 border-gray-300 shadow-sm transition-all rounded-full relative hover:bg-gray-100">
-                        <GraduationCap className="h-6 w-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-gray-800" />
+  return (
+    <div className="absolute top-10 right-10 z-50 cursor-pointer">
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger>
+          <div className="h-12 w-12 cursor-pointer duration-100 border-1 border-gray-300 shadow-sm transition-all rounded-full relative hover:bg-gray-100">
+            <GraduationCap className="h-6 w-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-gray-800" />
+          </div>
+        </DialogTrigger>
+        <DialogContent className="flex flex-col gap-3 min-w-[500px]">
+          {/* Heading */}
+          <DialogHeader>
+            <DialogTitle>
+              Your study journey
+            </DialogTitle>
+            <DialogDescription>
+              Keep studying to extend your streak and earn new badges
+            </DialogDescription>
+          </DialogHeader>
+
+          {/* Streak */}
+          <div className="flex flex-col gap-2 items-center justify-between pt-2">
+            <div className="flex flex-col items-center gap-4">
+              <div className="relative h-24 w-24">
+                {loading ? (
+                  <Skeleton className="w-[100px] h-[100px] rounded-full" />
+                ) : (
+                  <>
+                    <svg className="h-full w-full" viewBox="0 0 100 100">
+                      <circle
+                        className="stroke-primary/20"
+                        strokeWidth="10"
+                        fill="transparent"
+                        r="45"
+                        cx="50"
+                        cy="50"
+                      />
+                      <circle
+                        className="stroke-primary transition-all"
+                        strokeWidth="10"
+                        strokeLinecap="round"
+                        fill="transparent"
+                        r="45"
+                        cx="50"
+                        cy="50"
+                        strokeDasharray={`${(data.streak?.length || 0) * 10} 1000`}
+                        transform="rotate(-90 50 50)"
+                      />
+                    </svg>
+                    <div className="absolute inset-0 flex items-center justify-center">
+                      <span className="text-2xl font-bold text-primary">{data.streak?.length || 0}</span>
                     </div>
-                </DialogTrigger>
-                <DialogContent className="flex flex-col gap-3 min-w-[500px]">
-                    {/* Heading */}
-                    <DialogHeader>
-                        <DialogTitle>
-                            Your study journey
-                        </DialogTitle>
-                        <DialogDescription>
-                            Keep studying to extend your streak and earn new badges
-                        </DialogDescription>
-                    </DialogHeader>
+                  </>
+                )}
+              </div>
+              <div className="flex flex-col text-center">
+                <h3 className="text-lg font-semibold">
+                  {data.streak && data.streak.length > 0 ? `Your study streak` : `No study streak`}
+                </h3>
+                {loading ? (
+                  <Skeleton className="h-3 mt-1 w-full" />
+                ) : (
+                  <p className="text-sm text-gray-500">
+                    {data.streak && data.streak.length > 0 ? `${data.streak.length} day${data.streak.length > 1 ? 's' : ''} in a row` : `Start a streak`}
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <div className="grid grid-cols-7 gap-1">
+                {['M', 'T', 'W', 'T', 'F', 'S', 'S'].map((day, i) => (
+                  <div key={i} className="h-10 flex items-center justify-center">
+                    <span className="text-sm text-gray-500">{day}</span>
+                  </div>
+                ))}
+              </div>
+              <div className="grid grid-cols-7 gap-1">
+                {loading ? Array(14).fill(0).map((_, i) => (
+                  <div
+                    key={i}
+                    className="h-10 w-10 rounded-lg flex items-center justify-center"
+                  >
+                    <Skeleton className="h-10 w-10" />
+                  </div>
+                )) : adjustedStreakHistory.map((day, i) => {
+                  if (day === null) {
+                    return (
+                      <div
+                        key={i}
+                        className="h-10 w-10 rounded-lg bg-white border border-gray-200 flex items-center justify-center"
+                      >
+                        <Flame className="h-6 w-6 text-gray-200" />
+                      </div>
+                    );
+                  }
 
-                    {/* Streak */}
-                    <div className="flex flex-col gap-2 items-center justify-between pt-2">
-                        <div className="flex flex-col items-center gap-4">
-                            <div className="relative h-24 w-24">
-                                {loading ? (
-                                    <Skeleton className="w-[100px] h-[100px] rounded-full" />
-                                ) : (
-                                    <>
-                                        <svg className="h-full w-full" viewBox="0 0 100 100">
-                                            <circle
-                                                className="stroke-primary/20"
-                                                strokeWidth="10"
-                                                fill="transparent"
-                                                r="45"
-                                                cx="50"
-                                                cy="50"
-                                            />
-                                            <circle
-                                                className="stroke-primary transition-all"
-                                                strokeWidth="10"
-                                                strokeLinecap="round"
-                                                fill="transparent"
-                                                r="45"
-                                                cx="50"
-                                                cy="50"
-                                                strokeDasharray={`${(data.streak?.length || 0) * 10} 1000`}
-                                                transform="rotate(-90 50 50)"
-                                            />
-                                        </svg>
-                                        <div className="absolute inset-0 flex items-center justify-center">
-                                            <span className="text-2xl font-bold text-primary">{data.streak?.length || 0}</span>
-                                        </div>
-                                    </>
-                                )}
-                            </div>
-                            <div className="flex flex-col text-center">
-                                <h3 className="text-lg font-semibold">
-                                    {data.streak && data.streak.length > 0 ? `Your study streak` : `No study streak`}
-                                </h3>
-                                {loading ? (
-                                    <Skeleton className="h-3 mt-1 w-full" />
-                                ) : (
-                                    <p className="text-sm text-gray-500">
-                                        {data.streak && data.streak.length > 0 ? `${data.streak.length} day${data.streak.length > 1 ? 's' : ''} in a row` : `Start a streak`}
-                                    </p>
-                                )}
-                            </div>
-                        </div>
-                        <div className="flex flex-col">
-                            <div className="grid grid-cols-7 gap-1">
-                                {['M', 'T', 'W', 'T', 'F', 'S', 'S'].map((day, i) => (
-                                    <div key={i} className="h-10 flex items-center justify-center">
-                                        <span className="text-sm text-gray-500">{day}</span>
-                                    </div>
-                                ))}
-                            </div>
-                            <div className="grid grid-cols-7 gap-1">
-                                {loading ? Array(14).fill(0).map((_, i) => (
-                                    <div
-                                        key={i}
-                                        className="h-10 w-10 rounded-lg flex items-center justify-center"
-                                    >
-                                        <Skeleton className="h-10 w-10" />
-                                    </div>
-                                )) : adjustedStreakHistory.map((day, i) => {
-                                    if (day === null) {
-                                        return (
-                                            <div
-                                                key={i}
-                                                className="h-10 w-10 rounded-lg bg-white border border-gray-200 flex items-center justify-center"
-                                            >
-                                                <Flame className="h-6 w-6 text-gray-200" />
-                                            </div>
-                                        );
-                                    }
-
-                                    return (
-                                        <div
-                                            key={i}
-                                            className={`h-10 w-10 rounded-lg ${day.length > 0 ? 'bg-primary' : 'bg-primary/10'
-                                                } flex items-center justify-center`}
-                                        >
-                                            <Flame className={`h-6 w-6 ${day.length > 0 ? 'text-white' : 'text-primary/30'
-                                                }`} />
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        </div>
+                  return (
+                    <div
+                      key={i}
+                      className={`h-10 w-10 rounded-lg ${day.length > 0 ? 'bg-primary' : 'bg-primary/10'
+                        } flex items-center justify-center`}
+                    >
+                      <Flame className={`h-6 w-6 ${day.length > 0 ? 'text-white' : 'text-primary/30'
+                        }`} />
                     </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
 
-                    <Separator />
+          <Separator />
 
-                    {/* Achievements */}
-                    <div className="flex flex-col gap-3">
-                        <div>
-                            <p className="text-lg font-semibold">
-                                Your badges
-                            </p>
-                        </div>
-                    </div>
-                    {loading ? (
-                        <div className="grid grid-cols-3 gap-2 w-full">
-                            {Array(3).fill(0).map((_, idx) => (
-                                <div key={idx} className="p-3 rounded-md border border-gray-200 flex flex-col gap-3 items-center justify-between shadow-sm min-h-[130px]">
-                                    <Skeleton className="w-[75px] h-[75px] rounded-full" />
-                                    <div className="flex flex-col gap-1 w-full items-center justify-center">
-                                        <Skeleton className="h-3 w-full" />
-                                        <Skeleton className="h-2 w-1/2" />
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    ) : data.achievements && data.achievements.length > 0 ? (
-                        <div className="grid grid-cols-3 gap-2 w-full">
-                            {data.achievements?.map((achievement) => (
-                                <div key={achievement.id} className="p-2 rounded-md border border-gray-200 flex flex-col gap-1 items-center shadow-sm">
-                                    <Image
-                                        src={achievement.badgeUrl as string}
-                                        alt={achievement.name as string}
-                                        width={100}
-                                        height={100}
-                                        className="rounded-full border-gray-300"
-                                    />
-                                    <p className="font-semibold">{achievement.name}</p>
-                                    <p className="text-gray-500 text-sm">
-                                        {dayjs(achievement.achievedAt).format('MMM D, YYYY')}
-                                    </p>
-                                </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <div className="flex flex-col gap-1 items-center justify-center min-h-[100px] p-3 w-full mt-3">
-                            <p className='font-semibold text-lg'>
-                                No badges yet
-                            </p>
-                            <p className="text-sm text-gray-500 text-center max-w-[200px]">
-                                Keep studying to unlock more badges!
-                            </p>
-                        </div>
-                    )}
-                </DialogContent>
-            </Dialog>
-        </div>
-    )
+          {/* Achievements */}
+          <div className="flex flex-col gap-3">
+            <div>
+              <p className="text-lg font-semibold">
+                Your badges
+              </p>
+            </div>
+          </div>
+          {loading ? (
+            <div className="grid grid-cols-3 gap-2 w-full">
+              {Array(3).fill(0).map((_, idx) => (
+                <div key={idx} className="p-3 rounded-md border border-gray-200 flex flex-col gap-3 items-center justify-between shadow-sm min-h-[130px]">
+                  <Skeleton className="w-[75px] h-[75px] rounded-full" />
+                  <div className="flex flex-col gap-1 w-full items-center justify-center">
+                    <Skeleton className="h-3 w-full" />
+                    <Skeleton className="h-2 w-1/2" />
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : data.achievements && data.achievements.length > 0 ? (
+            <div className="grid grid-cols-3 gap-2 w-full">
+              {data.achievements?.map((achievement) => (
+                <div key={achievement.id} className="p-2 rounded-md border border-gray-200 flex flex-col gap-1 items-center shadow-sm">
+                  <Image
+                    src={achievement.badgeUrl as string}
+                    alt={achievement.name as string}
+                    width={100}
+                    height={100}
+                    className="rounded-full border-gray-300"
+                  />
+                  <p className="font-semibold">{achievement.name}</p>
+                  <p className="text-gray-500 text-sm">
+                    {dayjs(achievement.achievedAt).format('MMM D, YYYY')}
+                  </p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col gap-1 items-center justify-center min-h-[100px] p-3 w-full mt-3">
+              <p className='font-semibold text-lg'>
+                No badges yet
+              </p>
+              <p className="text-sm text-gray-500 text-center max-w-[200px]">
+                Keep studying to unlock more badges!
+              </p>
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
 }
